ddff50dfb7e818f28dfb150db0a7f8766cc8f03e
Fieldhunter <1677532160Yuan@gmail.com>
2020-04-06 12:49:34 +0800

    add a new function:start training epoch

--- cosine_annealing.py
+++ cosine_annealing.py
@@ -7,8 +7,9 @@ class CosineAnnealingScheduler(Callback):
     """Cosine annealing scheduler.
     """
 
-    def __init__(self, T_max, eta_max, eta_min=0, verbose=0):
+    def __init__(self, T_max, eta_max, init_epoch=0, eta_min=0, verbose=0):
         super(CosineAnnealingScheduler, self).__init__()
+        self.init_epoch = init_epoch
         self.T_max = T_max
         self.eta_max = eta_max
         self.eta_min = eta_min
@@ -17,7 +18,7 @@ class CosineAnnealingScheduler(Callback):
     def on_epoch_begin(self, epoch, logs=None):
         if not hasattr(self.model.optimizer, 'lr'):
             raise ValueError('Optimizer must have a "lr" attribute.')
-        lr = self.eta_min + (self.eta_max - self.eta_min) * (1 + math.cos(math.pi * epoch / self.T_max)) / 2
+        lr = self.eta_min + (self.eta_max - self.eta_min) * (1 + math.cos(math.pi * (epoch - self.init_epoch) / self.T_max)) / 2
         K.set_value(self.model.optimizer.lr, lr)
         if self.verbose > 0:
             print('\nEpoch %05d: CosineAnnealingScheduler setting learning '
